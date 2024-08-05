82f2db281f567e47e40cd37558eff2124baeca38
LittleChen12 <2827497158@qq.com>
2024-08-05 17:37:42 +0800

    learning

--- value/value/value.cpp
+++ value/value/value.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
-
-int main()
-{
-	int a = 10;
-
-	std::cout << " a = " << a << std::endl;
-
-	system("pause");
-
-	return 0;
-}
+#include <string>
+//int main1()
+//{
+//	double a = 10.01;
+//	int b;
+//	b = sizeof(a);
+//	std::cout << "" << a << std::endl;
+//	std::cout << "" << b << std::endl;
+//
+//	std::string str = "helloworld";
+//	std::cout << str <<std::endl;
+//
+//	int shuru;
+//	std::cout << "ÇëÊäÈë" << std::endl;
+//	std::cin >> shuru;
+//
+//	std::cout << shuru << std::endl;
+//
+//	system("pause");
+//	return 0;
+//}   
---
+++ value/value/算术.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int a = 10,b;
+	b = ++a * 100;
+	std::cout << b << std::endl;
+	system("pause");
+	return 0;
+}
